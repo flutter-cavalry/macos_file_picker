42280aef338b5647c9a903521a511039061d43f6
Mgen <m@mgenware.com>
2024-12-31 23:30:13 +0800

    Migrate to swift pm

--- macos/macos_file_picker/Sources/macos_file_picker/MacosFilePickerPlugin.swift
+++ macos/macos_file_picker/Sources/macos_file_picker/MacosFilePickerPlugin.swift
@@ -5,13 +5,14 @@ enum MacosFilePickerMode: Int { case file, folder, fileAndFolder, saveFile }
 
 public class MacosFilePickerPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "macos_file_picker", binaryMessenger: registrar.messenger)
+    let channel = FlutterMethodChannel(
+      name: "macos_file_picker", binaryMessenger: registrar.messenger)
     let instance = MacosFilePickerPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    guard let args = call.arguments as? Dictionary<String, Any> else {
+    guard let args = call.arguments as? [String: Any] else {
       result(FlutterError(code: "InvalidArgsType", message: "Invalid args type", details: nil))
       return
     }
@@ -44,8 +45,8 @@ public class MacosFilePickerPlugin: NSObject, FlutterPlugin {
         panel.canCreateDirectories = mode == .folder || mode == .fileAndFolder
         let res = panel.runModal()
         if res == .OK {
-          if (allowsMultiple) {
-            result(panel.urls.map{ urlToMap($0) })
+          if allowsMultiple {
+            result(panel.urls.map { urlToMap($0) })
           } else {
             if let url = panel.url {
               result([urlToMap(url)])
@@ -61,7 +62,7 @@ public class MacosFilePickerPlugin: NSObject, FlutterPlugin {
       result(FlutterMethodNotImplemented)
     }
   }
-  
+
   private func urlToMap(_ url: URL) -> [String: String] {
     return ["url": url.absoluteString, "path": url.path, "name": url.lastPathComponent]
   }
